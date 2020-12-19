f995e6d56734d5932f46fe786b9fbd2336e99e50
douglasmatias21 <71949172+douglasmatias21@users.noreply.github.com>
2020-12-19 12:15:26 -0700

    Delete Project 1.5 DFNB - Analyze History - Visualization and Presentation.sql

--- Tables/Project 1.5 DFNB - Analyze History - Visualization and Presentation.sql
+++
@@ -1,6 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-SELECT TOP (1000) [branch_id]
-      ,[Account_Qtd_by_Branch]
-      ,[Loan_Amount]
-      ,[Year]
-  FROM [DFNB2].[dbo].[Vbranch]
