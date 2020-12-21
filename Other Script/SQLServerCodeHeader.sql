2a6f58abe3213f7b7b5c526f6b8190a08a2b9aad
douglasmatias21 <71949172+douglasmatias21@users.noreply.github.com>
2020-12-21 02:38:49 -0700

    Added enhancements for LDSBC IT 240 - Project 1.4: DFNB - Analyze History - Create v1 of Script Repository

--- Other Script/SQLServerCodeHeader.sql
+++ Other Script/SQLServerCodeHeader.sql
@@ -8,7 +8,7 @@ MODIFICATION LOG:
 Ver      Date        Author        Description
 -----   ----------   -----------   -------------------------------------------------------------------------------
 1.0     11/21/2020   DOUGLAS MATIAS  1. Creation of the Script
-RUNTIME: 
+RUNTIME:  
 Approx. 1 min
 NOTES:
 Loading DFNB_STG_P1 - Project 1 database. Meaning creating all the tables inside the database.
